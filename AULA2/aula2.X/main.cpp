5321783774f9d42607cbb7baa82ff8b466e206e3
Daniel Cabral Correa <daniel.liboos@gmail.com>
2019-09-11 16:12:06 -0300

    falta terminar o main e testar

--- AULA2/aula2.X/main.cpp
+++ AULA2/aula2.X/main.cpp
@@ -47,8 +47,37 @@ void USART_Transmit( uint8_t data ){
     UDR0 = data;     					//Put data into buffer, sends the data
 }
 
+/*
+#define led 13
+#define B 39500000
+#define r10k 10000
+#define vin 10230000
+#define rinf 176
+unsigned long rntc = 0;
+unsigned long vout_d = 0;
+unsigned int tempk = 0;
+unsigned int lnr = 0;
+byte tempc = 0;
+
+digitalWrite(led, HIGH); // turn on pullup resistors
+  vout_d = analogRead(A1);
+  rntc = r10k*((vin/vout_d)-10000);
+  lnr = log(rntc/rinf)*100;
+  tempk = B/(lnr);
+  tempc = (tempk - 27315)/100;
+  digitalWrite(led, LOW); // turn on pullup resistors
+*/
 int main() {
 	ADC_Init();
     USART_Init(MYUBRR);
-    
+    void loop() {
+	  uint16_t vout_d = ADC_Read(0);
+	  float rntc = 10000.0*((1023.0/vout_d));
+	  lnr = log(rntc/rinf)*100;
+	  tempk = B/(lnr);
+	  tempc = (tempk - 27315)/100;
+	  digitalWrite(led, LOW); // turn on pullup resistors
+	  delay(2000);  
+	}
+	return 0;
 }
